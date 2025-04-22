8e79c8ee876b0f30a0623f19a1250233aacc52de
chuxiaojie <1186982585@qq.com>
2025-04-22 20:22:04 +0800

    feat: update the name of OpenAiCodeReview

--- openai-code-review-sdk/src/main/java/com/example/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/example/sdk/OpenAiCodeReview.java
@@ -5,7 +5,7 @@ package com.example.sdk;
  * @Date 2025/4/21 17:10
  * @Description:
  */
-public class OpenAICodeReview {
+public class OpenAiCodeReview {
 
     public static void main(String[] args) {
         System.out.println("测试执行");
