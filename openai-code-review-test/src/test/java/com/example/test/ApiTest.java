9a38c29f3c239445da797573aac5b25c1f56cd10
chuxiaojie <1186982585@qq.com>
2025-04-25 17:14:35 +0800

    feat: v2

--- openai-code-review-test/src/test/java/com/example/test/ApiTest.java
+++ openai-code-review-test/src/test/java/com/example/test/ApiTest.java
@@ -18,6 +18,8 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println("test");
+        System.out.println("test1");
+        System.out.println("test2");
+        System.out.println("test3");
     }
 }
