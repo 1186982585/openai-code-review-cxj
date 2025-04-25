d841553a10c1ca1fc980cb5e44778e110414ad2b
chuxiaojie <1186982585@qq.com>
2025-04-25 17:02:54 +0800

    feat: 检出代码

--- openai-code-review-sdk/src/main/java/com/example/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/example/sdk/OpenAiCodeReview.java
@@ -1,5 +1,10 @@
 package com.example.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @Author cxj
  * @Date 2025/4/21 17:10
@@ -7,8 +12,29 @@ package com.example.sdk;
  */
 public class OpenAiCodeReview {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("测试执行");
+
+        // 1.代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+
+        System.out.println("评审代码："+diffCode.toString());
+
+
     }
 
 }
