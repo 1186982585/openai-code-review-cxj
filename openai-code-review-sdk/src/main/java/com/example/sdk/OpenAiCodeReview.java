a2b6e27bb0980fb30d228f2d8f9da8983e0a6eb6
chuxiaojie <1186982585@qq.com>
2025-04-26 21:33:00 +0800

    feat: 代码评审 v4.2

--- openai-code-review-sdk/src/main/java/com/example/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/example/sdk/OpenAiCodeReview.java
@@ -1,13 +1,16 @@
 package com.example.sdk;
 
 import com.alibaba.fastjson2.JSON;
+import com.example.sdk.domain.model.ChatCompletionRequest;
 import com.example.sdk.domain.model.ChatCompletionSyncResponse;
+import com.example.sdk.domain.model.Model;
 import com.example.sdk.types.utils.BearerTokenUtils;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 /**
  * @Author cxj
@@ -69,8 +72,17 @@ public class OpenAiCodeReview {
                 "]" +
                 "}";
 
+        ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
+        chatCompletionRequest.setModel(Model.GLM_4_FLASH.getCode());
+
+        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>(){{
+            add(new ChatCompletionRequest.Prompt("user", "你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码如下:"));
+            add(new ChatCompletionRequest.Prompt("user", diffCode));
+
+        }});
+
         try(OutputStream os = connection.getOutputStream()){
-            byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+            byte[] input = JSON.toJSONString(chatCompletionRequest).getBytes(StandardCharsets.UTF_8);
             os.write(input);
         }
 
